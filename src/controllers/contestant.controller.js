0862a8e93eb27816bf87175bf79af5a477eadd3f
sescobar-2020031 <sescobar-2020031@kinal.edu.gt>
2022-08-23 22:48:28 -0600

    Se agregaron otros cambios

--- src/controllers/contestant.controller.js
+++ src/controllers/contestant.controller.js
@@ -3,7 +3,7 @@
 const Contestant = require('../models/contestant.model');
 
 exports.testContestant = (req, res) => {
-    return res.send({ message: 'Test is running in Contestant' })
+    return res.send({ message: 'El test en -Contestant- es funcional' });
 }
 
 exports.saveContestant = async (req, res) => {
@@ -21,7 +21,7 @@ exports.saveContestant = async (req, res) => {
             studentCareer: params.studentCareer,
             poetryGenre: params.poetryGenre,
             enrollmentDate: new Date()
-        }
+        };
 
         /** Valida que venga la data obligatoria */
         let keys = Object.keys(data), msg = '';
@@ -34,28 +34,28 @@ exports.saveContestant = async (req, res) => {
         /** VALIDACIONES DEL CARNET */
 
         /** Valida que el carnet tenga 6 caracteres */
-        const lengthCarnet = data.carnet.length
-        if (lengthCarnet !== 6) return res.status(400).send({ message: 'El carnet tiene que tener una longitud de 6 dígitos' })
+        const lengthCarnet = data.carnet.length;
+        if (lengthCarnet !== 6) return res.status(400).send({ message: 'El carnet tiene que tener una longitud de 6 dígitos' });
 
         /** Valida que el carnet no contenga 0 */
         for (let letter of data.carnet) {
-            if (letter == 0) return res.status(400).send({ message: 'El carnet no debe de contener 0' })
+            if (letter == 0) return res.status(400).send({ message: 'El carnet no debe de contener 0' });
         }
 
         /** Valida que el primer caracter sea A o a*/
         if (data.carnet[0] !== 'A' && data.carnet[0] !== 'a') {
-            return res.status(400).send({ message: 'El carnet debe de contener una "A" o "a" en el primer carácter' })
+            return res.status(400).send({ message: 'El carnet debe de contener una "A" o "a" en el primer carácter' });
         }
 
         /** Valida que el tercer caracter sea 5 */
         if (data.carnet[2] != 5) {
-            return res.status(400).send({ message: 'El carnet debe de contener un "5" en el tercer carácter' })
+            return res.status(400).send({ message: 'El carnet debe de contener un "5" en el tercer carácter' });
         }
 
         /** Valida que el ultimo caracter sea 1,3 o 9 */
         const lastCharacter = data.carnet.charAt(lengthCarnet - 1);
         if (lastCharacter != 1 && lastCharacter != 3 && lastCharacter != 9) {
-            return res.status(400).send({ message: 'El carnet debe de contener un "1","3" o "9" en el último carácter' })
+            return res.status(400).send({ message: 'El carnet debe de contener un "1","3" o "9" en el último carácter' });
         }
 
         /** Valida que el concursante sea mayor a 17 años */
@@ -73,7 +73,7 @@ exports.saveContestant = async (req, res) => {
         const birthDay = birthDate.getDate() + 1;
 
         /** Cálculo */
-        let age = currentYear - birthYear
+        let age = currentYear - birthYear;
         if (currentMonth < birthMonth) {
             age--;
         } else if (currentMonth == birthMonth) {
@@ -81,7 +81,7 @@ exports.saveContestant = async (req, res) => {
                 age--;
             }
         }
-        if (age < 18) return res.status(400).send({ message: 'Tienes que ser mayor de 17 años para participar' })
+        if (age < 18) return res.status(400).send({ message: 'Tienes que ser mayor de 17 años para participar' });
         data.age = age;
 
         /** Validaciones de Fechas para Declamar */
@@ -90,7 +90,7 @@ exports.saveContestant = async (req, res) => {
             if (currentWeekDay == 5) dayToDeclaim = currentDay + 10;
             else if (currentWeekDay == 6) dayToDeclaim = currentDay + 9;
             else dayToDeclaim = currentDay + 8;
-            data.declamationDate = new Date(currentDate).setDate(dayToDeclaim)
+            data.declamationDate = new Date(currentDate).setDate(dayToDeclaim);
         } else if (lastCharacter == '3' && data.poetryGenre == 'Epico') {
             dayToDeclaim = new Date(currentYear, currentMonth + 1, 0).getDate();
             data.declamationDate = new Date(currentDate).setDate(dayToDeclaim);
@@ -102,9 +102,9 @@ exports.saveContestant = async (req, res) => {
             else if (currentWeekDay == 4) dayToDeclaim = currentDay + 1;
             else if (currentWeekDay == 5) dayToDeclaim = currentDay + 7;
             else dayToDeclaim = currentDay + 6;
-            data.declamationDate = new Date(currentDate).setDate(dayToDeclaim)
+            data.declamationDate = new Date(currentDate).setDate(dayToDeclaim);
         }
-        
+
         let contestant = new Contestant(data);
         await contestant.save();
         return res.send({ message: 'Inscrito Exitosamente', contestant });
@@ -118,11 +118,11 @@ exports.saveContestant = async (req, res) => {
 exports.getContestants = async (req, res) => {
     try {
         const participants = await Contestant.find();
-        if (participants.length === 0) return res.status(400).send({ message: 'There are no participants' });
-        return res.send({ message: 'Found participants: ', participants })
+        if (participants.length === 0) return res.status(400).send({ message: 'No se encontraron participantes' });
+        return res.send({ message: 'Participantes encontrados: ', participants });
     } catch (er) {
         console.log(err);
-        return res.status(500).send({ err, message: 'Error getting contestants' })
+        return res.status(500).send({ err, message: 'Error obteniendo participantes' });
     }
 }
 
@@ -130,10 +130,10 @@ exports.getContestant = async (req, res) => {
     try {
         const participationId = req.params.id;
         const contestantParticipation = await Contestant.findOne({ _id: participationId });
-        if (!contestantParticipation) return res.status(400).send({ message: 'Participation not found' });
-        return res.send({ message: 'Found participant: ', contestantParticipation })
+        if (!contestantParticipation) return res.status(400).send({ message: 'Participante no encontrado' });
+        return res.send({ message: 'Participante encontrado: ', contestantParticipation });
     } catch (err) {
         console.log(err);
-        return res.status(500).send({ err, message: 'Error getting contestant' })
+        return res.status(500).send({ err, message: 'Error obteniendo participante' });
     }
 }
